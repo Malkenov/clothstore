42d985bf9161b29ac99ea07a23ac33ffadecfe57
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2025-10-11 00:07:55 +0500

    5

--- src/main/java/com/example/clothstore/clothstore/entity/Order.java
+++ src/main/java/com/example/clothstore/clothstore/entity/Order.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Data
 @Entity
-
+@Table(name = "orders")
 public class Order {
 
     @Id
--- src/main/java/com/example/clothstore/clothstore/entity/User.java
+++ src/main/java/com/example/clothstore/clothstore/entity/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 @Entity
-
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
